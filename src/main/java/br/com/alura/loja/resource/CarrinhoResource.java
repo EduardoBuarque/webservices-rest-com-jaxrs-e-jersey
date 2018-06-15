ec6cb54b13c64b4361a32aa3880099f7e4000f79
EduardoBuarque <sensit.info@gmail.com>
2018-06-15 18:01:56 -0300

    Continuacao Aulua 5

--- src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -1,9 +1,12 @@
 package br.com.alura.loja.resource;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
@@ -14,6 +17,13 @@ public class CarrinhoResource {
 	public String busca() {
 		Carrinho carrinho = new CarrinhoDAO().busca(1L);
 		return carrinho.toXML();
+	}	
+	
+	@POST @Produces(MediaType.APPLICATION_XML)
+	public String adicionaCarrinho(String conteudo){
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		new CarrinhoDAO().adiciona(carrinho);
+		return "<sucesso></sucesso>";
 	}
 	
 }
