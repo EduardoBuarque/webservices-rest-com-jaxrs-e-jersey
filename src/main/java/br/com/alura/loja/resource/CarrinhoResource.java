ed14db2c57308776e96c8565b0119cb8d3131483
EduardoBuarque <sensit.info@gmail.com>
2018-06-20 09:43:25 -0300

    Aula 10

--- src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -3,6 +3,7 @@ package br.com.alura.loja.resource;
 import java.net.URI;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -32,5 +33,13 @@ public class CarrinhoResource {
 		URI uri = URI.create("/carrinhos/" + carrinho.getId());
 		Response response = Response.created(uri).build();
 		return response;
+	}
+	
+	@Path("{id}/produtos/{produtoId}")
+	@DELETE
+	public Response removeProduto(@PathParam("id") long id, @PathParam("produtoId")long produtoId){
+		Carrinho carrinho = new CarrinhoDAO().busca(id);
+		carrinho.remove(produtoId);
+		return Response.ok().build();
 	}
 }
